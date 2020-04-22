b9e705898a30cbb82b587b8d918320c2ff6d4506
elon <eea76@msn.com>
2020-04-22 10:13:20 -0700

    shit

--- channelsproj/asgi.py
+++ channelsproj/asgi.py
@@ -2,9 +2,13 @@
 
 import os
 from channels.layers import get_channel_layer
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelsproj.settings")
 channel_layer = get_channel_layer()
+django.setup()
+application = get_default_application()
 
 ########################
 
