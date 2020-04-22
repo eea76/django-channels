5fa59ed073c768eb7ed11daa9badaea936cfc88e
elon <eea76@msn.com>
2020-04-22 09:52:13 -0700

    version of asgi

--- channelsproj/asgi.py
+++ channelsproj/asgi.py
@@ -1,17 +1,17 @@
 # old version of this file
 
-# import os
-# import channels.asgi
+import os
+import channels.asgi
 
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
-# channel_layer = channels.asgi.get_channel_layer()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelsproj.settings")
+channel_layer = channels.asgi.get_channel_layer()
 
 ########################
 
-import os
-import django
-from channels.routing import get_default_application
+# import os
+# import django
+# from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelsproj.settings")
-django.setup()
-application = get_default_application()
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelsproj.settings")
+# django.setup()
+# application = get_default_application()
