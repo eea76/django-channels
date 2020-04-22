c181ab2e23f60195ee68cb199027f699082a27b5
elon <eea76@msn.com>
2020-04-21 19:04:14 -0700

    asgi insanity fucking insanity

--- channelsproj/asgi.py
+++ channelsproj/asgi.py
@@ -5,3 +5,5 @@ from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "channelsproj.settings")
 django.setup()
 application=get_default_application()
+
+channel_layer = channels.asgi.get_channel_layer()
