53c6f3da7164a9de8f1f2018d2284e0fecea6c68
elon <eea76@msn.com>
2020-04-22 09:21:33 -0700

    some shit

--- channelsproj/routing.py
+++ channelsproj/routing.py
@@ -1,10 +1,15 @@
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.urls import path
+from django.urls import path, re_path
 from notifier.consumers import NoseyConsumer, SentenceConsumer
+from channels.http import AsgiHandler
 
 application = ProtocolTypeRouter({
     "websocket": URLRouter([
         path("notifications/", NoseyConsumer),
         path("sentences/", SentenceConsumer),
-    ])
+
+        # AsgiHandler is "the rest of Django" - send things here to have Django
+        # views handle them.
+        re_path("^", AsgiHandler),
+    ]),
 })
