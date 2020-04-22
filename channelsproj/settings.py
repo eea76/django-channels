e94a60b6daab35f20437898f10fa92efd08d8738
elon <eea76@msn.com>
2020-04-21 19:02:04 -0700

    try this

--- channelsproj/settings.py
+++ channelsproj/settings.py
@@ -147,13 +147,9 @@ LOGOUT_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
